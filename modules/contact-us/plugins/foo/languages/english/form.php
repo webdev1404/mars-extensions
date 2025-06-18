5cc7d87acbf7f96978cb2dc70f27ec24f9457b9d
Adrian Razvan Chis <sargon1404@gmail.com>
2025-06-18 16:53:55 +0300

    Original files

--- modules/contact-us/plugins/foo/languages/english/form.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-	'mod_test' => 'zzzzzzzzzzzzzz22222222224444',
-	'mod_test23' => 'zzzzzzwwwwwwzzzzzzzz22222222224444'
-];
--- modules/contact-us/templates/test.php
+++
@@ -1,4 +0,0 @@
-xcvcxvcxvcxccxcxcx<br>
-{{ contact-us.mod_test }}
-<br>xxxxx<br>
-{{ contact-us.mod_test23 }}
